7df59bef420005f0656faf09903a4a57a6835083
Joe Del Balzo <jdelbalzo99@gmail.com>
2024-10-14 13:50:46 -0400

    scripts for next 14

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+// next.config.js
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
+
+if (process.env.STATIC_EXPORT === "true") {
+  nextConfig.output = "export";
+}
 
 module.exports = nextConfig;
